27270622457f884219a3658478a988ad5df4cf0d
Nestor Boero <boero29@hotmail.com>
2020-08-06 16:02:15 -0300

    Clase tema 3, formularios + formulario GET

---
+++ Tema-03/form_get_server.php
@@ -0,0 +1,22 @@
+<?php
+    $_REQUEST["name"];
+    $_REQUEST["surname"];
+    $_REQUEST["email"];
+    $_REQUEST["population"];
+    $_REQUEST["province"];
+    $_REQUEST["edad"];
+    //$_REQUEST["recomendation0"];
+    //$_REQUEST["recomendation1"];
+    //$_REQUEST["recomendation2"];
+    //$_REQUEST["recomendation3"];
+    $_REQUEST["opinion"];
+    $_REQUEST["sugerence"];
+    $_REQUEST["browserTime"];
+?>
+
+
+<html>
+   <body>      
+       <p><?php echo "Hola ". $_REQUEST["name"] . " " . $_REQUEST["surname"] ?></p>
+   </body>
+</html>
